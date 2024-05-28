72e310dd35ae8bf700cf7561fa8fd4d984050c5c
Shubham <shubhamkumar7112002@gmail.com>
2024-05-28 23:29:29 +0530

    remove s from playlists

--- src/app.js
+++ src/app.js
@@ -35,6 +35,6 @@ app.use("/api/v1/videos",videoRouter)
 app.use("/api/v1/likes",likeRouter);
 app.use("/api/v1/comments",commentRouter);
 app.use("/api/v1/subscriptions",subscriptionRouter);
-app.use("/api/v1/playlists",playlistRouter);
+app.use("/api/v1/playlist",playlistRouter);
 
 export{app};
